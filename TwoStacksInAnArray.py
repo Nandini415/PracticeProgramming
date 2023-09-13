9bf892d60b42353b6539119ae7847ffa4f61980c
Nandini415 <91522033+Nandini415@users.noreply.github.com>
2023-09-13 14:54:41 +0000

    2 stacks prgm modified

--- TwoStacksInAnArray.py
+++ TwoStacksInAnArray.py
@@ -16,6 +16,7 @@ class twoStacks:
     if self.top2 < self.size-1:
       self.top2=self.top2+1
       self.arr[self.top2]=x
+      print(self.arr)
     else:
       print("Stack2 overflow")
 
@@ -38,14 +39,13 @@ class twoStacks:
       return -1
 
 if __name__ == '__main__':
-  s=twoStacks(7)
-  s.push1(1)
-  s.push2(2)
-  s.push2(3)
-  s.push2(6)
-  s.push1(19)
-  s.push1(3)
-  s.push1(7)
-  print(s.pop1())
-  print(s.pop1())
-  print(s.pop2())
+    ts = twoStacks(5)
+    ts.push1(5)
+    ts.push2(10)
+    ts.push2(15)
+    ts.push1(11)
+    ts.push2(7)
+      
+    print("Popped element from stack1 is : " + str(ts.pop1()))
+    ts.push2(40)
+    print("Popped element from stack2 is : " + str(ts.pop2()))
